9dd5f298672b482112fe30245646e76a915a4197
Markus D <markusdrx@gmail.com>
2022-05-18 18:07:52 +0200

    changed useState usage for useReducer

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
+
+const reducer = (state, action) => {
+  if (action.type === ACTIONS.INCREMENT) {
+    return { count: state.count + 1 };
+  } else {
+    return { count: state.count - 1 };
+  }
+};
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  // The array will have the state and a function called dispatch (sets action) that will call the reducer function
+  // useReducer uses 2 parameters to start: a reducer (function that we perform on our state to get new state) and an initial value:
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+    dispatch({ type: ACTIONS.INCREMENT });
   };
 
   const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
+    dispatch({ type: ACTIONS.DECREMENT });
   };
 
   return (
     <>
       <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}>-</button>
     </>
   );
